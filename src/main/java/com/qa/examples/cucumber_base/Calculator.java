4764d8b8ac08cada3f8a21fb9832181b85c71173
David <davidacosta25081@yahoo.com>
2021-06-15 11:31:25 -0500

    Add functions that implement substraction,division and multiplication

--- src/main/java/com/qa/examples/cucumber_base/Calculator.java
+++ src/main/java/com/qa/examples/cucumber_base/Calculator.java
@@ -22,17 +22,14 @@ public class Calculator {
 	}
 	
 	public double subtract(double a, double b) {
-		// TODO: Implement
-		return 0.0d;
+	  return a-b;
 	}
 	
 	public double divide(double a, double b) {
-		// TODO: Implement
-		return 0.0d;
+	  return a/b;
 	}
 	
 	public double multiply(double a, double b) {
-		// TODO: Implement
-		return 0.0d;
+	  return a*b;
 	}
 }
--- src/test/java/com/qa/examples/cucumber_base/step_definitions/CanCalculateNumbersCorrectly.java
+++ src/test/java/com/qa/examples/cucumber_base/step_definitions/CanCalculateNumbersCorrectly.java
@@ -15,7 +15,7 @@ public class CanCalculateNumbersCorrectly {
 
 	@Given("a calculator")
 	public void aCalculator() {
-		calculator = new Calculator();
+		this.calculator = new Calculator();
 	}
 
 	@When("{double} and {double} are added")
@@ -23,8 +23,31 @@ public class CanCalculateNumbersCorrectly {
 	    this.actualResult = this.calculator.add(leftOperand, rightOperand);
 	}
 	
+	
+	@When("{double} and {double} are substracted")
+	public void and_are_substracted(Double double1, Double double2) {
+	  this.actualResult = calculator.subtract(double1, double2);    
+	}
+
+	
+	
+	@When("{double} is divided by {double}")
+	public void is_divided_by(Double double1, Double double2) {
+	  this.actualResult = calculator.divide(double1, double2);
+	    
+	}
+
+	@When("{double} multiplies {double}")
+	public void multiplies(Double double1, Double double2) {
+		this.actualResult = calculator.multiply(double1, double2);
+	}
+
+	
+	
 	@Then("the result should be {double}")
 	public void theResultShouldBe(Double expectedResult) {
 	    assertEquals(expectedResult, actualResult, 7);
 	}
+
+
 }
